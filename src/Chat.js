5db17dcae5b3d2e7cd7e529940d862767d2ca114
galina matyasheva <galina.matyashev@gmail.com>
2018-10-21 14:39:22 +0300

    initial commit

---
+++ src/Chat.js
@@ -0,0 +1,71 @@
+import React from "react";
+import io from "socket.io-client";
+
+
+class Chat extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            username: '',
+            message: '',
+            messages: []
+        };
+
+        this.socket = io('localhost:8080');
+
+        this.sendMessage = ev => {
+            ev.preventDefault();
+            this.socket.emit('SEND_MESSAGE', {
+                author: this.state.username,
+                message: this.state.message
+            });
+            this.setState({message: ''});
+        };
+
+        const addMessage = data => {
+            console.log("add message data: " + data.author + data.message);
+            this.setState({messages: [...this.state.messages, data]});
+            console.log(this.state.messages);
+        };
+
+        this.socket.on('RECEIVE_MESSAGE', function(data){
+            addMessage(data);
+        });
+    }
+
+    render(){
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-4">
+                        <div className="card">
+                            <div className="card-body">
+                                <div className="card-title">Глобальный чат</div>
+                                <hr/>
+                                <div className="messages">
+                                    {this.state.messages.map(message => {
+                                        return (
+                                            <div><span class="colortext">{message.author}</span>: {message.message}</div>
+                                        )
+                                    })}
+                                </div>
+                            </div>
+                            <div className="card-footer">
+                                <input type="text" placeholder= "Имя пользователя" className="form-control"
+                                       value={this.state.username}
+                                       onChange={ev => this.setState({username: ev.target.value})} />
+                                <br/>
+                                <input type="text" placeholder="Сообщение" className="form-control"
+                                       value={this.state.message} onChange={ev => this.setState({message: ev.target.value})}/>
+                                <br/>
+                                <button onClick={this.sendMessage} className="btn btn-primary form-control">Отправить</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Chat;
